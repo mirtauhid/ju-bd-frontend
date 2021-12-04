468f296fac8eef4dc62959ce9e1c7c8bd0a7b9e9
MIR TAUHIDUL ISLAM <dev.tauhidul@gmail.com>
2021-12-05 03:31:18 +0600

    project setup completion commit

---
+++ src/views/Donor.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Donor = () => {
+  return <div></div>;
+};
+
+export default Donor;
---
+++ src/views/Donors.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Donors = () => {
+  return <div></div>;
+};
+
+export default Donors;
